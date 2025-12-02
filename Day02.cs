3eb653f4bebedd146a3499c3e882571863015292
Dan Burgener <dan.burgener@gmail.com>
2025-12-01 23:01:49 -0700

    Day Two complete

--- Day02.cs
+++ Day02.cs
@@ -8,13 +8,69 @@
         public async Task<long> One()
         {
             var data = await Common.ReadFile(_fileDayName, "One");
-            return data.Count();
+            List<long> invalidIds = new();
+            foreach(var item in data[0].Split(","))
+            {
+                var splitItem = item.Split("-");
+                var low = long.Parse(splitItem[0]);
+                var high = long.Parse(splitItem[1]);
+                invalidIds.AddRange(GetInvalidIds(low, high));
+            }
+            return invalidIds.Sum();
         }
 
         public async Task<long> Two()
         {
             var data = await Common.ReadFile(_fileDayName, "Two");
-            return data.Count();
+            List<long> invalidIds = new();
+            foreach (var item in data[0].Split(","))
+            {
+                var splitItem = item.Split("-");
+                var low = long.Parse(splitItem[0]);
+                var high = long.Parse(splitItem[1]);
+                invalidIds.AddRange(GetInvalidIdsV2(low, high));
+            }
+            return invalidIds.Sum();
+        }
+
+        private List<long> GetInvalidIds(long low, long high)
+        {
+            List<long> invalidIds = [];
+            for (long currentNumber = low; currentNumber <= high; currentNumber++)
+            {
+                string currentNumberString = currentNumber.ToString();
+                int length = currentNumberString.Length;
+                if (length % 2 == 0)
+                {
+                    int halfLength = length / 2;
+                    if (currentNumberString[..halfLength] == currentNumberString[halfLength..])
+                    {
+                        invalidIds.Add(currentNumber);
+                    }
+                }
+            }
+            return invalidIds;
+        }
+
+        private List<long> GetInvalidIdsV2(long low, long high)
+        {
+            List<long> invalidIds = [];
+            for (long currentNumber = low; currentNumber <= high; currentNumber++)
+            {
+                string currentNumberString = currentNumber.ToString();
+                int length = currentNumberString.Length;
+                for (var i = 1; i < length; i++)
+                {
+                    var chunks = currentNumberString.Chunk(i);
+                    var stringChunks = chunks.Select(c => int.Parse(c));
+                    if (stringChunks.Distinct().Count() == 1)
+                    {
+                        invalidIds.Add(currentNumber);
+                        break;
+                    }
+                }
+            }
+            return invalidIds;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 ﻿using AdventOfCode2025;
 
-IDay dayToRun = new Day01();
+IDay dayToRun = new Day02();
 
 Console.WriteLine(dayToRun.GetName());
+System.Diagnostics.Stopwatch stopwatch = new();
+stopwatch.Start();
 Console.WriteLine($"One: {await dayToRun.One()}");
+stopwatch.Stop();
+Console.WriteLine($"One Time: {stopwatch.ElapsedMilliseconds}");
+stopwatch.Restart();
 Console.WriteLine($"Two: {await dayToRun.Two()}");
+stopwatch.Stop();
+Console.WriteLine($"Two Time: {stopwatch.ElapsedMilliseconds}");
